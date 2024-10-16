8c63df38e1386c4e3875835effadd3d4b7ddb21b
Eliel_Estudante <eliel.r.silva@ba.estudante.senai.br>
2024-10-16 21:33:40 +0000

    Etapa 1

---
+++ main/models/functions/grafico_terminal.py
@@ -0,0 +1,12 @@
+
+
+NOME_LOGO = "      Eliel Studio's"
+
+def logo():
+    print("\n= = = = = = = = = = = = =")
+    print(NOME_LOGO)
+    print("= = = = = = = = = = = = =\n")
+
+def linha():
+    print("= = = = = = = = = = = = = = = = = = = = = = = = = =\n")
+
